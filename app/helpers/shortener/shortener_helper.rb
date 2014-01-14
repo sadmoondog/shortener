b21b2a218634e353b6f2a6bcc1bb7ede58c66fd6
Oleg Minaev <oleg.minaev@gmail.com>
2014-01-14 22:28:53 +0200

    added params for URLs

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -6,4 +6,9 @@ module Shortener::ShortenerHelper
     shortener ? URI.join(root_url, Shortener.clean_url_prefix, shortener.unique_key).to_s : url
   end
 
+  def short_url_param(url, param)
+    shortener = Shortener::ShortenedUrl.generate!(url)
+    shortener ? URI.join(root_url, Shortener.clean_url_prefix,  "/#{shortener.unique_key}", "?"+param.map{|key, val| "#{key}=#{val}"}.join("&")) : url
+  end
+
 end
