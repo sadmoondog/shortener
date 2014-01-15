51fa9e10d4cefc4713a0df0c2cd3a55d6094c0fd
Oleg Minaev <oleg.minaev@gmail.com>
2014-01-15 13:16:21 +0200

    updated tracking code

--- app/middlewares/shortener_redirect_middleware.rb
+++ app/middlewares/shortener_redirect_middleware.rb
@@ -9,15 +9,13 @@ class ShortenerRedirectMiddleware
   def call(env)
 
     if (env["PATH_INFO"] =~ ::Shortener.match_url) && (shortener = ::Shortener::ShortenedUrl.find_by_unique_key($1))
+
       shortener.track env if ::Shortener.tracking
 
       uid = 'u0'
-
       uid ='u'+env['rack.session']['warden.user.user.key'][1][0].to_s rescue nil
       uid += CGI.escape(env['QUERY_STRING']) rescue nil
       track! :click, {:identity=>UserIdentity.new(env['rack.session'][Evercookie.hash_name_for_saved][:uid]), :values=>[1]} rescue nil
-
-
 
       [301, {'Location' => shortener.url.gsub('sofitsmeuserid', uid)}, []]
     else
